5ccf0e3461e9b257f97bca88b678d510022761ab
Sridhar Pedhamanishi <peddamanishisridhar@gmail.com>
2025-11-15 23:06:33 +0530

    Add Netlify SPA redirect for client-side routing

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // your backend
+  baseURL: "https://voting-application-uwf6.onrender.com/", // your backend
 });
 
 api.interceptors.request.use((config) => {
